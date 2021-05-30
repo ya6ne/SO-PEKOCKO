c1e558aff8d94581c930cf16bdbb89865401de49
yass <ya6n@hotmail.fr>
2021-05-30 15:50:26 +0200

    VF

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,18 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+var CryptoJS = require("crypto-js");
+
+var key = CryptoJS.enc.Hex.parse("000102030405060708090a0b0c0d0e0f"); /* key et iv pour CryptoJs */
+var iv = CryptoJS.enc.Hex.parse("101112131415161718191a1b1c1d1e1f");
+
 
 exports.signup = (req, res, next) => {
+  var cryptedMail = CryptoJS.AES.encrypt(req.body.email, key, { iv: iv }).toString();
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
-            email: req.body.email,
+            email: cryptedMail,
             password: hash
         });
         user.save()
@@ -17,13 +23,15 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+  var cryptedMail = CryptoJS.AES.encrypt(req.body.email, key, { iv: iv }).toString();
+    User.findOne({ email: cryptedMail })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
+
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
