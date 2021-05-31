0c2f75dc60fc3020de9d908cf0eefdcefe40b3b3
yass <ya6n@hotmail.fr>
2021-06-01 00:16:56 +0200

    vppf

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('../middleware/multer-c');
+const auth = require('../middleware/auth');  /* inversé avec sauceCtrl */
 const sauceCtrl = require('../controllers/sauce');
-const auth = require('../middleware/auth');
+
 
 router.post('/',auth, multer, sauceCtrl.createSauce);
 router.put('/:id',auth, multer, sauceCtrl.updateSauce);
 router.delete('/:id',auth, sauceCtrl.deleteSauce);
-router.get('/',auth, sauceCtrl.getAllSauces);
-router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/',auth , sauceCtrl.getAllSauces);
+router.get('/:id', auth , sauceCtrl.getOneSauce);
 
-router.post('/:id/like', sauceCtrl.likeSauce);
+router.post('/:id/like', auth , sauceCtrl.likeSauce);
 
 module.exports = router;
